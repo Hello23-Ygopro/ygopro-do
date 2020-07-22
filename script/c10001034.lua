dcb50f8701dd34bc645712a70329195b44be5bf7
Hello23-Ygopro <loleh32@hotmail.com>
2020-07-22 02:02:39 +0200

    fix Merchant
    Fixed a player not getting a coin.

--- script/c10001034.lua
+++ script/c10001034.lua
@@ -1,29 +1,26 @@
 --Merchant
 local scard,sid=aux.GetID()
 function scard.initial_effect(c)
-	--draw, add action, add coin
+	--draw, add action, get effect
 	aux.AddActionEffect(c,scard.op1)
-	if not scard.global_check then
-		scard.global_check=true
-		local ge1=Effect.CreateEffect(c)
-		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge1:SetCode(EVENT_CHAINING)
-		ge1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		ge1:SetOperation(scard.regop1)
-		Duel.RegisterEffect(ge1,0)
-	end
 end
---draw, add action, add coin
-function scard.regop1(e,tp,eg,ep,ev,re,r,rp)
-	local rc=re:GetHandler()
-	if rp==tp and rc:IsCode(CARD_SILVER) then
-		Duel.RegisterFlagEffect(rp,sid,RESET_PHASE+PHASE_END,0,1)
-	end
-end
+--draw, add action, get effect
 function scard.op1(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Draw(tp,1,REASON_EFFECT)
 	Duel.AddAction(tp,1)
-	if Duel.GetFlagEffect(tp,sid)==1 then
+	--add coin
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetOperation(scard.op2)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end
+--add coin
+function scard.op2(e,tp,eg,ep,ev,re,r,rp)
+	local rc=re:GetHandler()
+	if rp==tp and rc:IsCode(CARD_SILVER) then
+		Duel.Hint(HINT_CARD,0,sid)
 		Duel.AddCoin(tp,1)
 	end
 end
